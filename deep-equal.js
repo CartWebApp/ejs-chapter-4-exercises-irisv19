10780fcd779662231462140a0d7db7e0b188de7b
irisv19 <143832578+irisv19@users.noreply.github.com>
2024-02-19 19:15:42 -0800

    reviwed

--- deep-equal.js
+++ deep-equal.js
@@ -1,6 +1,21 @@
 function deepEqual(value, reference) {
-  // Add code.
+  if (a === b) return true;
+  
+  if (a == null || typeof a != "object" ||
+      b == null || typeof b != "object") return false;
+
+  let keysA = Object.keys(a), keysB = Object.keys(b);
+
+  if (keysA.length != keysB.length) return false;
+
+  for (let key of keysA) {
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+  }
+
+  return true;
 }
+
+
 
 
 
--- reverse-arrays.js
+++ reverse-arrays.js
@@ -1,10 +1,21 @@
-function reverseArray(arr) {
-  // Add code.
+function reverseArray(array) {
+  let output = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    output.push(array[i]);
+  }
+  return output;
 }
 
-function reverseArrayInPlace(arr) {
-  // Add code.
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
 }
+
 
 
 
